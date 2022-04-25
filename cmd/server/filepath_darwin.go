ec59ef5cfd3aa616e9924c3d26f8868e84da44bd
Camilo Zapata <camilo.zapata@nebulae.com.co>
2022-04-25 17:01:23 -0500

    add defaults options

--- cmd/server/filepath_darwin.go
+++ cmd/server/filepath_darwin.go
@@ -2,3 +2,7 @@ package main
 
 var filenameKey = "/key.pem"
 var filenameCert = "/cert.pem"
+
+func init() {
+	flag.StringVar(&certpath, "certpath", "$HOME", "path to certificate file, if this option wasn't defined the application will create a new temporal certificatee")
+}
--- cmd/server/filepath_linux.go
+++ cmd/server/filepath_linux.go
@@ -1,4 +1,10 @@
 package main
 
+import "flag"
+
 var filenameKey = "/key.pem"
 var filenameCert = "/cert.pem"
+
+func init() {
+	flag.StringVar(&certpath, "certpath", "$HOME", "path to certificate file, if this option wasn't defined the application will create a new temporal certificatee")
+}
