deb3649868ccb51638995fdc4c91f3f491e633f7
Camilo Zapata <camilo.zapata@nebulae.com.co>
2024-04-02 10:35:36 -0500

    add "-force-iso14443-3" option

--- internal/pcsc/reader/connectReader_linux.go
+++ internal/pcsc/reader/connectReader_linux.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/ebfe/scard"
 	"github.com/nebulaengineering/pcsc-agnostic-daemon/internal/pcsc/context"
+	"github.com/nebulaengineering/pcsc-agnostic-daemon/utils"
 )
 
 var forceIso14443_3 bool
@@ -51,18 +52,37 @@ func ConnectReader(c *context.Context, reader string) (*Reader, error) {
 		if err != nil {
 			return nil, err
 		}
-		resp1, err := direct.Control(0x42000000+2079, []byte{0x23, 0x00})
+		apdu := []byte{0x23, 0x00}
+		if utils.Debug {
+			fmt.Printf("request CONTROL APDU: [% 02X]\n", apdu)
+		}
+		resp1, err := direct.Control(0x42000000+2079, apdu)
 		if err != nil {
 			return nil, err
 		}
+		if utils.Debug {
+			fmt.Printf("response CONTROL APDU: [% 02X]\n", resp1)
+		}
+
 		if len(resp1) > 0 && resp1[len(resp1)-1] != (0x8F&func() byte {
 			if forceIso14443_3 {
 				return 0x7F
 			}
 			return 0xFF
 		}()) {
-			if _, err := direct.Control(0x42000000+2079, []byte{0x23, 0x01, 0x8F}); err != nil {
+			apdu := []byte{0x23, 0x01, 0x8F & func() byte {
+				if forceIso14443_3 {
+					return 0x7F
+				}
+				return 0xFF
+			}()}
+			if utils.Debug {
+				fmt.Printf("request CONTROL APDU: [% 02X]\n", apdu)
+			}
+			if resp, err := direct.Control(0x42000000+2079, apdu); err != nil {
 				return nil, err
+			} else if utils.Debug {
+				fmt.Printf("response CONTROL APDU: [% 02X]\n", resp)
 			}
 		}
 		direct.Disconnect(scard.LeaveCard)
